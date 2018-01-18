59511b04d638cf3f64795458f35dea008b725bd9
Richard Horton <richardhorton97@outlook.com>
2018-01-18 14:13:29 +0000

    Added wordSelect Function
    Created a function to select a random word from the word list and returns it so it can be saved

--- HangmanMain.py
+++ HangmanMain.py
@@ -13,3 +13,17 @@ class player():
         self.lives = 5
 
     
+def wordSelect():
+    '''Selects a word from the wordList file and stores it to be used in the game'''
+    #wordNum range starts from 1 to ensuure that the source URL in the wordList file is never selected as the word
+    wordNum = rand.randint(1, 1000)
+    wordList = open("wordList.txt", "r")
+    for i in range(wordNum+1):
+        if i == wordNum:
+            word = wordList.readline()
+        else:
+            wordList.readline()
+    wordList.close()
+    return word
+
+
