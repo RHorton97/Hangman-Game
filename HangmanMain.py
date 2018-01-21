5d4c3fd64fd59784826247e32ac30f780722eae8
Richard Horton <richardhorton97@outlook.com>
2018-01-21 14:04:10 +0000

    Game can now be replayed
    Game can now be replayed without restarting, it also no longer has to call as many functions from the Game class as they are handled within the class itself and not by external code

--- HangmanMain.py
+++ HangmanMain.py
@@ -14,32 +14,38 @@ print("Chosen Word: " + runGame.word)                                        #
 #runGame.beginGame()                                                          #
 ###############################################################################
 
-runGame.beginGame()
-playGame = True
-while playGame == True:
-    validType = False
-    while validType == False:
-        guessType = input("Press 1 to guess a letter or press 2 to guess the word. ")
-        try:
-            guessType = int(guessType)
-        except ValueError:
-            continue
-        if guessType == 1 or 2:
-            validType = True
+playAgain = True
+while playAgain == True:
+    runGame.beginGame()
+    playGame = True
+    while playGame == True:
+        validChoice = False
+        while validChoice == False:
+            guessType = input("Press 1 to guess a letter or press 2 to guess the word. ")
+            try:
+                guessType = int(guessType)
+            except ValueError:
+                continue
+            if guessType == 1 or 2:
+                validChoice = True
 
-    if guessType == 1:
-        runGame.letterGuess()
-    elif guessType == 2:
-        runGame.wordGuess()
-    else:
-        raise ValueError("Invalid guess type selection")
+        if guessType == 1:
+            runGame.letterGuess()
+        elif guessType == 2:
+            runGame.wordGuess()
+        else:
+            raise ValueError("Invalid guess type selection")
 
-    if runGame.guessWrong == True:
-        runGame.guessDown()
-        runGame.guessWrong = False
-        if runGame.guesses <= 0:
-            runGame.gameLost()
-            playGame = False
-    elif runGame.playerWins == True:
-        runGame.gameWon()
-        playGame = False
+        if runGame.gameOver == True:
+            validChoice = False
+            while validChoice == False:
+                playAgain = input("\nDo you want to play again? y/n  ")
+                if playAgain.lower() == "n":
+                    validChoice = True
+                    playGame = False
+                    playAgain = False
+                elif playAgain.lower() == "y":
+                    validChoice = True
+                    playGame = False
+                    playAgain = True
+                    runGame.resetGame()
